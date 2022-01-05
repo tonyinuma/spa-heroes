8868176c85916f0f76f190376b67ad23165e9936
Tony ID <tony.inuma@gmail.com>
2022-01-05 17:59:05 -0500

    Feat: Navigate back using replace

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 const LoginScreen = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate('/', {
+            replace: true
+        });
+    }
+
     return (
-        <div>
-            <h1>LoginScreen</h1>
+        <div className="container mt-5">
+            <h1>Login</h1>
+            <br/>
+
+            <button className="btn btn-primary" onClick={handleLogin}>Sign In</button>
         </div>
     );
 };
