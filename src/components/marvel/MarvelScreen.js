80a702e478a3bb5bffca197b32d15034855b3087
Tony ID <tony.inuma@gmail.com>
2022-01-05 23:29:59 -0500

    Add: Component to list Marvel Heroes

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {HeroList} from "../hero/HeroList";
 
 const MarvelScreen = () => {
     return (
         <div>
-            <h1>MarvelScreen</h1>
+            <HeroList/>
         </div>
     );
 };
