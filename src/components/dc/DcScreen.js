b0bc7c9316972acbb0c2af9d236e876749d1c6f7
Tony ID <tony.inuma@gmail.com>
2022-01-05 23:29:48 -0500

    Add: Component to list DC Heroes

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {HeroList} from "../hero/HeroList";
 
 const DcScreen = () => {
     return (
         <div>
-            <h1>DcScreen</h1>
+            <HeroList publisher={'DC Comics'}/>
         </div>
     );
 };
