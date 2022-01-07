d85ad5946e57c2981b067102d32b7808d4c471b3
Tony ID <tony.inuma@gmail.com>
2022-01-06 19:44:45 -0500

    Feat: Hero Card Design and redirect

--- src/components/hero/HeroCard.js
+++ src/components/hero/HeroCard.js
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 export const HeroCard = ({hero}) => {
 
     const imagePath = `/assets/images/heroes/${hero.id}.jpg`;
@@ -5,13 +7,24 @@ export const HeroCard = ({hero}) => {
     return (
         <div className="col">
             <div className="card bg-dark text-white">
-                <img src={imagePath} className="card-img-top" alt={hero.superhero}/>
-                <div className="card-img-overlay">
-                    <h5 className="card-title text-info px-2" style={{background: 'black', borderRadius: '5px'}}>
-                        <b>{hero.superhero}</b>
-                        <p style={{color: 'gray'}}>{hero.alter_ego}</p>
-                    </h5>
-                </div>
+                <Link to={`/hero/${hero.id}`}>
+                    <img src={imagePath} className="card-img-top" alt={hero.superhero}/>
+                    <div className="card-img-overlay">
+                        <h4 className="card-title text-info px-2 position-absolute bottom-0 start-0">
+                            <b style={{background: 'black'}}>{hero.superhero}</b>
+                            <p className="text-success" style={{background: 'black'}}>{hero.alter_ego}</p>
+                            {
+                                <p style={{
+                                    background: 'black',
+                                    color: 'white',
+                                    fontSize: '12px'
+                                }}>
+                                    {hero.alter_ego !== hero.characters && `${hero.characters} /`} {hero.first_appearance}
+                                </p>
+                            }
+                        </h4>
+                    </div>
+                </Link>
             </div>
         </div>
     );
