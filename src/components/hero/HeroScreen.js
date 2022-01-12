9cb6522f41a477fbcb74a2ea1e28815e089f4a24
Tony ID <tony.inuma@gmail.com>
2022-01-12 16:53:55 -0500

    Feat: get url params and get data by id, validate data

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,7 +1,16 @@
+import {Navigate, useParams} from "react-router-dom";
+import {getHeroById} from "../../selectors/getHeroById";
+
 const HeroScreen = () => {
+
+    const {id} = useParams();
+    const hero = getHeroById(id)
+
+    if (!hero) return <Navigate to="/"/>
+
     return (
         <h1>
-            Hero Screen
+            {hero.superhero}
         </h1>
     );
 };
