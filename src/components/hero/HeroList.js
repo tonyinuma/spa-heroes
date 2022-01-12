3d7abc645cb1a7cb137abcccfe51dd8aafc49f06
Tony ID <tony.inuma@gmail.com>
2022-01-12 17:29:46 -0500

    Add: Use memo getHeroByPublisher function

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,9 +1,10 @@
 import {getHeroByPublisher} from "../../selectors/getHeroByPublisher";
 import {HeroCard} from "./HeroCard";
+import {useMemo} from "react";
 
 export const HeroList = ({publisher = 'Marvel Comics'}) => {
 
-    const heroes = getHeroByPublisher(publisher);
+    const heroes = useMemo(() => getHeroByPublisher(publisher), [publisher]);
 
     return (
         <div className="col-md-12">
