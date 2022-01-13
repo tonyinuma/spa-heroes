ef5817c870a2147330f60908f7fa7d07df558d2b
Tony ID <tony.inuma@gmail.com>
2022-01-13 12:41:15 -0500

    Add: Selector to get Heroes by Name

---
+++ src/selectors/getHeroesByName.js
@@ -0,0 +1,7 @@
+import {heroes} from '../data/heroes';
+
+export const getHeroByName = (name = '') => {
+
+    name = name.toLowerCase();
+    return heroes.filter(hero => hero.superhero.toLowerCase().includes(name));
+}
