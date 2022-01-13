07e71b0d28c05e173120d6dbb8395f5d3511c7b9
Tony ID <tony.inuma@gmail.com>
2022-01-13 15:44:28 -0500

    Add: validation when no exists data to get

--- src/selectors/getHeroesByName.js
+++ src/selectors/getHeroesByName.js
@@ -2,6 +2,8 @@ import {heroes} from '../data/heroes';
 
 export const getHeroByName = (name = '') => {
 
+    if (name.length === 0) return [];
+
     name = name.toLowerCase();
     return heroes.filter(hero => hero.superhero.toLowerCase().includes(name));
 }
