b928f105218d423c09582b9144b20d5cd390fd2c
Tony ID <tony.inuma@gmail.com>
2022-01-13 12:41:15 -0500

    Feat: Form search design

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 const SearchScreen = () => {
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log("submit");
+    }
+
     return (
-        <div>
-            <h1>SearchScreen</h1>
-        </div>
+        <>
+            <div className="row container">
+                <div className="col-md-5">
+                    <h4>Search a Hero</h4>
+                    <form onSubmit={(e) => {
+                        handleSearch(e)
+                    }}>
+                        <input type="text" placeholder="Write his name here ..."
+                               className="form-control"
+                               name="searchText"
+                               autoComplete="off"/>
+                        <button type="submit" className="btn btn-outline-primary mt-2">
+                            Search
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 };
 
