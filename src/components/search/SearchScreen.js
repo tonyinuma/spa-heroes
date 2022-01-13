c05b86fbfb54ef6dd198a2e2177c4ea419639c9d
Tony ID <tony.inuma@gmail.com>
2022-01-13 12:49:52 -0500

    Feat: implements useForm to get form data and fix submit form

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {useForm} from "../../hooks/useForm";
 
 const SearchScreen = () => {
+
+    const [formValues, handleInputChange] = useForm({
+        searchText: ''
+    });
+
+    const {searchText} = formValues;
 
     const handleSearch = (e) => {
         e.preventDefault();
         console.log("submit");
+        console.log(formValues);
     }
 
     return (
@@ -12,13 +20,13 @@ const SearchScreen = () => {
             <div className="row container">
                 <div className="col-md-5">
                     <h4>Search a Hero</h4>
-                    <form onSubmit={(e) => {
-                        handleSearch(e)
-                    }}>
+                    <form onSubmit={handleSearch}>
                         <input type="text" placeholder="Write his name here ..."
                                className="form-control"
                                name="searchText"
-                               autoComplete="off"/>
+                               autoComplete="off"
+                               value={searchText}
+                               onChange={handleInputChange}/>
                         <button type="submit" className="btn btn-outline-primary mt-2">
                             Search
                         </button>
