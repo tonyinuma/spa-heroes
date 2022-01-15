0845f2bf54b83050f988b463173c1e986533e51e
Tony ID <tony.inuma@gmail.com>
2022-01-15 10:28:09 -0500

    Add: Auth Context

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,7 +1,10 @@
 import AppRouter from "./routers/AppRouter";
+import {AuthContext} from "./auth/authContext";
 
 const HeroesApp = () => {
-    return <AppRouter/>;
+    return <AuthContext.Provider value={{name: 'Tony'}}>
+        <AppRouter/>
+    </AuthContext.Provider>;
 };
 
 export default HeroesApp;
