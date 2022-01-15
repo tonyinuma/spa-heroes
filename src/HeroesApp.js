436a5fe181ed3736bbc83a26011622055b298199
Tony ID <tony.inuma@gmail.com>
2022-01-15 11:10:18 -0500

    Add: Reducer using context

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,8 +1,20 @@
 import AppRouter from "./routers/AppRouter";
 import {AuthContext} from "./auth/authContext";
+import {useReducer} from "react";
+import {authReducer} from "./auth/authReducer";
+
+const init = () => {
+    return {
+        logged: true,
+        name: 'Tony'
+    }
+}
 
 const HeroesApp = () => {
-    return <AuthContext.Provider value={{name: 'Tony'}}>
+
+    const [user, dispatch] = useReducer(authReducer, {}, init);
+
+    return <AuthContext.Provider value={{user, dispatch}}>
         <AppRouter/>
     </AuthContext.Provider>;
 };
