72f29a0a773202b005be29bf53b65c63ebbe7906
Tony ID <tony.inuma@gmail.com>
2022-01-16 12:15:47 -0500

    Feat: Private Routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,16 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginScreen from "../components/login/LoginScreen";
 import DashboardRouter from "./DashboardRouter";
+import {PrivateRoute} from "./PrivateRoute";
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<LoginScreen/>}/>
-                <Route path="/*" element={<DashboardRouter/>}/>
+                <Route path="/*" element={<PrivateRoute>
+                    <DashboardRouter/>
+                </PrivateRoute>}/>
             </Routes>
         </BrowserRouter>
     );
