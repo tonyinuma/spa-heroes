e4bf9b2433f7e1c53aabc24418b3197aad3e2d9e
Tony ID <tony.inuma@gmail.com>
2022-01-16 12:25:19 -0500

    Feat: Public Routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,12 +2,15 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginScreen from "../components/login/LoginScreen";
 import DashboardRouter from "./DashboardRouter";
 import {PrivateRoute} from "./PrivateRoute";
+import {PublicRoute} from "./PublicRoute";
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<LoginScreen/>}/>
+                <Route path="/login" element={<PublicRoute>
+                    <LoginScreen/>
+                </PublicRoute>}/>
                 <Route path="/*" element={<PrivateRoute>
                     <DashboardRouter/>
                 </PrivateRoute>}/>
